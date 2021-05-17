4f1bc681445e789cc54535056efa4d301c516c4f
22836252 <ken99899@gmail.com>
2021-05-17 14:22:53 +0800

    updatetemplate
    updatetemplate

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import button from '@/components/button'
+// import button from '@/components/button'
+// import ctof from '@/page/ctof'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: '/hello',
+      name: 'hello',
       component: HelloWorld
     },
-    {
-      path: '/button',
-      name: 'button',
-      component: button
-    }
+    // {
+    //   path: '/button',
+    //   name: 'button',
+    //   component: button
+    // },
+    // {
+    //   path: '/ctof',
+    //   name: 'ctof',
+    //   component: ctof
+    // },
+    // router 轉址
+    { path: '/*', redirect: '/hello' }
   ]
 })
